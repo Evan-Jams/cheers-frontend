1052c3248d3a881f1f5b67317a942f1403a38824
Evan James <thevan777@gmail.com>
2020-03-17 12:02:08 -0400

    delete beer functionality

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ export default class App extends React.Component {
     }
     this.handleAddBeer = this.handleAddBeer.bind(this)
     this.getBeers = this.getBeers.bind(this)
+    this.deleteBeer = this.deleteBeer.bind(this)
   }
   handleAddBeer(beer){
       const copyBeers = [beer, ...this.state.beers]
@@ -43,6 +44,22 @@ export default class App extends React.Component {
     }
   }
 
+  async deleteBeer(id){
+      try {
+          let response = await fetch(`${baseURL}/beers/${id}`, {
+              method: 'DELETE'
+          })
+          await response.json()
+          const foundBeer = this.state.beers.findIndex(beer =>
+          beer._id === id)
+          const copyBeers = [...this.state.beers]
+          copyBeers.splice(foundBeer, 1)
+          this.setState({beers: copyBeers})
+      } catch (e) {
+          console.error(e);
+      }
+  }
+
   render() {
     return(
       <div>
@@ -52,7 +69,7 @@ export default class App extends React.Component {
         {
           this.state.beers.map((beer, i) => {
             return(
-              <li key={i}><Beer key={i} beer={beer}/></li>
+              <li key={i}><Beer key={i} beer={beer} deleteBeer={this.deleteBeer}/></li>
             )
           })
         }
--- src/components/Beer.js
+++ src/components/Beer.js
@@ -9,6 +9,7 @@ class Beer extends React.Component {
       <h4>{this.props.beer.abv}</h4>
       <h2><a href={this.props.beer.url}>{this.props.beer.brewery}</a></h2>
       <p>{this.props.beer.description}</p>
+      <button onClick={()=> this.props.deleteBeer(this.props.beer._id)}>Delete</button>
 
       </div>
     )
