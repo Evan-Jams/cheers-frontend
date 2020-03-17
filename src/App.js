412fab85ad4bc2d8884538a233b85cb8d9433457
Evan James <thevan777@gmail.com>
2020-03-17 10:48:14 -0400

    added form and components folder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
 import React from 'react';
+import NewBeer from './components/NewBeerForm.js'
+
+
+let baseURL = ''
+
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3003'
+} else {
+  baseURL = 'https://cheers-backend.herokuapp.com/'
+}
+
 
 export default class App extends React.Component {
   constructor(props){
@@ -6,11 +17,20 @@ export default class App extends React.Component {
     this.state = {
       beers: []
     }
+    this.handleAddBeer = this.handleAddBeer.bind(this)
   }
+  handleAddBeer(beer){
+      const copyBeers = [beer, ...this.state.beers]
+      this.setState({
+          beers: copyBeers
+      })
+  }
+
   render() {
     return(
       <div>
         Hello from App
+        <NewBeer baseURL={baseURL} handleAddBeer={this.handleAddBeer}/>
       </div>
     )
   }
